a70a0a549cbcf68cfbb1dffa585cd5a6201afc75
natelloyd1 <nathanaellloyd@live.com>
2020-03-09 12:07:20 +0000

    add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
+import Nav from "./Nav"; 
+
+
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+function App() {  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <Router>
+    <Nav /> 
+    <Switch>
+      <Articles />
+    </Switch>
+  </Router>
+  );  
+  }
 
+  const Home = () => (
+    <div>
+      <h1>My first homepage on React</h1>
+    </div>
+  ); 
+
+// export our component
 export default App;
